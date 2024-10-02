f241a22f9e1cdc378c17393931b223d063a3256d
Marco Zagaria <marcozagaria@live.com>
2024-10-02 15:09:41 +0200

    esercizio 2

--- src/Main.java
+++ src/Main.java
@@ -10,12 +10,27 @@ public class Main {
         products.add(new Product(21561, "lavatrice", "elettrodomestici", 200.0));
         products.add(new Product(34165, "libro 3", "books", 9.9));
         products.add(new Product(41165, "libro 4", "books", 17.7));
+        products.add(new Product(51961, "Action Figure 1", "boys", 120.0));
+        products.add(new Product(61531, "Action Figure 2", "boys", 90.0));
+        products.add(new Product(81651, "Action Figure 3", "boys", 200.0));
 
-
-        List<Product> filterProducts = products.stream()
+        System.out.println("-----------------------esercizio 1------------------");
+        List<Product> filterBooks = products.stream()
                 .filter(product -> "books".equals(product.getCategory()) && product.getPrice() > 100)
                 .toList();
 
-        filterProducts.forEach(product -> System.out.println("nome del libro: " + product.getName() + " al costo di: " + product.getPrice() + "€"));
+        filterBooks.forEach(product ->
+                System.out.println("nome del libro: " + product.getName() + " al costo di: " + product.getPrice() + "€"));
+
+
+        System.out.println("-----------------------esercizio 3------------------");
+        List<Product> filterBoys = products.stream()
+                .filter(product -> "boys".equals(product.getCategory())).map(product -> {
+                    product.setPrice(product.getPrice() * 0.9);
+                    return product;
+                }).toList();
+
+        filterBoys.forEach(product ->
+                System.out.println("nome action figure: " + product.getName() + " al prezzo scontato del 10%: " + product.getPrice() + "€"));
     }
 }
